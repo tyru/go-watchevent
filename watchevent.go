13be951ebe35a5d010f46ddf8b95bedc443a54bc
tyru <tyru.exe@gmail.com>
2017-08-23 01:42:59 +0900

    Parse args, load config

--- watchevent.go
+++ watchevent.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 
+	"gopkg.in/yaml.v2"
+
 	"github.com/go-fsnotify/fsnotify"
+	"github.com/soh335/sliceflag"
 )
 
 var version = "x.y.z"
@@ -22,10 +27,29 @@ func Main() int {
 		return 1
 	}
 
+	var configFile string
+	flag.StringVar(&configFile, "config", "", "config file")
+	flag.StringVar(&configFile, "c", "", "config file")
+	// TODO
+	// var directories = sliceflag.String(flag.CommandLine, "directory", []string{}, "directory to be watched")
+	var directories = sliceflag.String(flag.CommandLine, "d", []string{}, "directory to be watched")
+	flag.Parse()
+
+	if configFile == "" {
+		fmt.Fprintln(os.Stderr, "-config option was not specified")
+		return 2
+	}
+	config, err := loadConfig(configFile)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, configFile, ": Could not load config file")
+		return 3
+	}
+	fmt.Println("[DEBUG] Config:", config)
+
 	watcher, err := fsnotify.NewWatcher()
 	if err != nil {
-		log.Fatal(err)
-		return 2
+		fmt.Fprintln(os.Stderr, "Could not initialize watcher")
+		return 4
 	}
 	defer watcher.Close()
 
@@ -34,21 +58,67 @@ func Main() int {
 	go poll(watcher, done)
 
 	// Watch the specified directory
-	for i := 1; i < len(os.Args); i++ {
-		if file, err := os.Stat(os.Args[i]); err != nil || !file.IsDir() {
-			fmt.Fprintln(os.Stderr, os.Args[i], ": given string does not exist or not a directory")
-			return 3
+	for _, dir := range *directories {
+		fmt.Println("[DEBUG] Dir: ", dir)
+		if file, err := os.Stat(dir); err != nil || !file.IsDir() {
+			fmt.Fprintln(os.Stderr, dir, ": given string does not exist or not a directory")
+			return 5
 		}
 	}
-	for i := 1; i < len(os.Args); i++ {
-		err := watchDirsUnder(os.Args[i], watcher)
+	for _, dir := range *directories {
+		err := watchDirsUnder(dir, watcher)
 		if err != nil {
-			fmt.Fprintln(os.Stderr, os.Args[i], ": Could not watch directory")
-			return 4
+			fmt.Fprintln(os.Stderr, dir, ": Could not watch directory")
+			return 6
 		}
 	}
 
 	return <-done
+}
+
+type Config struct {
+	OnWrite  string `yaml:"on_write"`
+	OnCreate string `yaml:"on_create"`
+	OnRemove string `yaml:"on_remove"`
+	OnRename string `yaml:"on_rename"`
+	OnChmod  string `yaml:"on_chmod"`
+	Action   []Action
+}
+
+type Action struct {
+	Name  string
+	Sleep string
+	Run   string
+}
+
+func loadConfig(filename string) (*Config, error) {
+	buf, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	err = yaml.Unmarshal(buf, &config)
+	if err != nil {
+		return nil, err
+	}
+
+	err = validateConfig(&config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func validateConfig(config *Config) error {
+	if len(config.Action) == 0 {
+		return errors.New("No actions were defined")
+	}
+
+	// TODO
+
+	return nil
 }
 
 func poll(watcher *fsnotify.Watcher, done chan int) {
